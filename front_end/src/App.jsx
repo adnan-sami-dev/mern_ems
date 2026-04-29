d22ff56c7cf77778eee597466ab4e62c80eae779
Adnan <sami.adnandev@gmail.com>
2026-04-29 16:02:45 -0600

    setup routes

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -1,10 +1,16 @@
 import {Toaster} from "react-hot-toast"
-import {Route, Routes} from "react-router-dom"
+import {Route, Routes, Navigate} from "react-router-dom"
 import LoginPage from "./pages/login"
 import LayoutPage from "./pages/layout"
 import DashboardPage from "./pages/dashboard"
 import EmployeesPage from "./pages/employees"
 import AttendancePage from "./pages/attendance"
+import PayslipsPage from "./pages/payslips"
+import PrintPayslipPage from "./pages/printpayslip"
+import SettingsPage from "./pages/settings"
+
+
+import "./index.css"
 
 const App = () => {
 
@@ -13,15 +19,19 @@ const App = () => {
       <Toaster />
       <Routes>
         <Route path="/login" element = { <LoginPage /> } />
+ 
         <Route element={<LayoutPage />}>
           <Route path="/dashboard" element={<DashboardPage />}/>
           <Route path="/employees" element={<EmployeesPage />}/>
           <Route path="/attendance" element={<AttendancePage/>}/>
           <Route path="/leave" element={<DashboardPage />}/>
-          <Route path="/dashboard" element={<DashboardPage />}/>
-
+          <Route path="/payslips" element={<PayslipsPage />}/>
+          <Route path="/settings" element={<SettingsPage />}/>
         </Route>
 
+        <Route path="/print/payslips/:id" element = { <PrintPayslipPage /> } />
+
+        <Route path="*" element={ <Navigate to="/dashboard" replace/>} />
       </Routes>
     </>
   )
--- front_end/src/pages/layout.jsx
+++ front_end/src/pages/layout.jsx
@@ -1,8 +1,15 @@
-
+import {Outlet} from "react-router-dom"
 
 const LayoutPage = () => {
   return (
-    <div>layout</div>
+    <div className = "flex h-screen" >
+      <p> Sidebar </p>
+      <main className = " flex-1 overflow-y-auto">
+        <div className = " p-4 pt-16 sm: p-6 sm: pt-6 lg: p-8 max-w-400 mx-auto " >
+          <Outlet />
+        </div>
+      </main>
+    </div>
   )
 }
 
