6b705e767c57ba219493a666c1e8679cc5f17ae3
A <sami.adnandev@gmail.com>
2026-05-05 22:43:30 -0600

    login page portals list update

--- front_end/src/pages/login.jsx
+++ front_end/src/pages/login.jsx
@@ -1,6 +1,22 @@
-
+import {ShieldIcon, UserIcon} from "lucide-react"
+import {Link} from "react-router-dom"
 
 const LoginPage = () => {
+
+  const PortalSelections = [
+    {
+      to: "/login/admin",
+      text: "Admin",
+      description: "manage employees, departments, payroll and system configurations",
+      icon: ShieldIcon
+    },
+    {
+      to: "/login/employee",
+      text: "Employee",
+      description: "view profile, track attendance, request time off, and access payslips",
+      icon: UserIcon
+    }
+  ]
   return (
     <main className="min-h-screen flex flex-col md:flex-row" >
       <div className="hidden md:flex flex-col flex-1 bg-black text-white items-center justify-center">
@@ -13,12 +29,24 @@ const LoginPage = () => {
       <div className="flex-1 bg-gray-300 flex items-center justify-center" >
         {/* Header */}
         <div className= "text-center md:text-left">
-          <h2 className="text-3xl mb-3 font-medium text-slate-900">Welcome</h2>
+          <h2 className="text-3xl mb-2 font-medium text-slate-900">Welcome</h2>
           <p className="text-slate-500">Select your portal . . .</p>
         </div>
 
         {/* Portal list */}
-        <div></div>
+        <div className="flex gap-1 flex-col">
+          {
+            PortalSelections.map( (portal) => {
+              <Link key={portal.to} to={portal.to}>
+                <button className="block w-full p-3 sm:p-6 border rounded-lg hover:bg-slate-400 hover:cursor-pointer">
+                  <h3>
+                    {portal.text}
+                  </h3>
+                </button>
+              </Link>
+            })
+          }
+        </div>
 
         {/* Footer */}
         <div></div>
