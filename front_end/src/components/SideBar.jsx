87f53be548a1fb42371e347625fa6a00fd948c33
adnan-sami-dev <98939536+adnan-sami-dev@users.noreply.github.com>
2026-07-19 20:47:05 -0600

    finished UI for SideBar

---
+++ front_end/src/components/SideBar.jsx
@@ -0,0 +1,61 @@
+import {useLocation} from "react-router-dom"
+import { useEffect, useState } from "react"
+import { dummyProfileData } from "../assets/assets"
+import { MenuIcon, X } from "lucide-react"
+import SideBarContent from "./SideBarContent"
+
+const SideBar = () => {
+    const {pathname} = useLocation() // Get the current path from the location object -> for highlighting on sidebar
+    const [username, setUsername] = useState("")
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+    useEffect(() => {
+        setUsername(dummyProfileData.username + " " + dummyProfileData.lastname)
+    }, []) // sets the username on component mount - once only!
+
+    useEffect(() => {
+        setMobileMenuOpen(false)
+    }, [pathname]) // close the mobile menu when the path changes
+
+    return (
+        <div>
+            {/* Mobile Menu Button */}
+            <button onClick={() => setMobileMenuOpen(!mobileMenuOpen)} 
+                className = {`lg:hidden fixed top-4 left-4 z-50 p-5 bg-slate-500 text-slate-300 rounded-2xl ${mobileMenuOpen ? "opacity-0 pointer-events-none" : "opacity-100 pointer-events-auto"} transition-opacity duration-300`}>
+                {mobileMenuOpen ? <X size={20} />: <MenuIcon size={20} />}
+            </button>
+
+            {/* MobileBackdrop */}
+            <div onClick={() => setMobileMenuOpen(false)}
+                className={`lg:hidden fixed inset-0 z-30 bg-black/30 backdrop-blur-sm transition-opacity duration-300 
+                ${
+                        mobileMenuOpen
+                        ? "opacity-100 pointer-events-auto"
+                        : "opacity-0 pointer-events-none"
+                }`}
+            />
+
+            {/* Mobile Menu */}
+            <aside
+                className={`lg:hidden fixed top-0 left-0 h-full w-3/4 md:w-1/2 z-40
+                    bg-slate-900 flex flex-col gap-8
+                    text-slate-300 text-lg
+                    transition-transform duration-300 ease-in-out
+                    ${
+                    mobileMenuOpen
+                        ? "translate-x-0"
+                        : "-translate-x-full"
+                    }`}
+            >
+                <SideBarContent className = "z-50" />
+            </aside>
+
+            {/* Desktop Menu */}
+            <aside className = "hidden lg:flex flex-col h-full w-70 bg-slate-900 text-slate-200" >
+                <SideBarContent className = "z-50" />
+            </aside>
+        </div>
+    )
+}
+
+export default SideBar
