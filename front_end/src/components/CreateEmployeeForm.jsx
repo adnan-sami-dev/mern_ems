35db969d3d3ac83fc87ab6a9bd757ecfa8cf8bf2
Adnan <sami.adnandev@gmail.com>
2026-08-11 05:57:14 -0600

    basic structure for employee form modal UI

--- front_end/src/components/CreateEmployeeForm.jsx
+++ front_end/src/components/CreateEmployeeForm.jsx
@@ -2,7 +2,57 @@ import React from 'react'
 
 const CreateEmployeeForm = () => {
   return (
-    <div>CreateEmployeeForm</div>
+    <div>
+      {/* backdrop */}
+      <div />
+
+      {/* form modal */}
+      <div>
+
+        {/* personal info */}
+        <div>
+
+          {/* input - first name */}
+          <div>
+            <label></label>
+            <input />
+          </div>
+
+          {/* input - last name */}
+          <div>
+            <label></label>
+            <input />
+          </div>
+
+          {/* input - email */}
+          <div>
+            <label></label>
+            <input />
+          </div>
+
+          {/* input - phone */}
+          <div>
+            <label></label>
+            <input />
+          </div>
+
+          {/* input - date joined */}
+          <div>
+            <label></label>
+            <input />
+          </div>
+
+          {/* input - additional info */}
+          <div>
+            <label></label>
+            <textarea rows={3} className="resize-none" />
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
